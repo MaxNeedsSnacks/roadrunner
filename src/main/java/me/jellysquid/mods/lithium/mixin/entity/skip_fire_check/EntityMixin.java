836a26c521ddabf520a54114dd1cc736d3169fcc
malte0811 <malte0811@web.de>
2021-05-18 15:40:06 +0200

    Fix: Reimplement skipFireTestIfResultDoesNotMatter, modified for the Forge patch of the relevant code

--- src/main/java/me/jellysquid/mods/lithium/mixin/entity/skip_fire_check/EntityMixin.java
+++ src/main/java/me/jellysquid/mods/lithium/mixin/entity/skip_fire_check/EntityMixin.java
@@ -1,9 +1,8 @@
 package me.jellysquid.mods.lithium.mixin.entity.skip_fire_check;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,20 +18,19 @@ public abstract class EntityMixin {
     @Shadow
     protected abstract int getBurningDuration();
 
-    // TODO: Re-evaluate if this is needed because forge strips this method
-    /*@Redirect(
+    @Redirect(
             method = "move",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/World;method_29556(Lnet/minecraft/util/math/Box;)Ljava/util/stream/Stream;"
+                    target = "Lnet/minecraft/util/math/BlockPos;stream(Lnet/minecraft/util/math/Box;)Ljava/util/stream/Stream;"
             )
     )
-    private Stream<BlockState> skipFireTestIfResultDoesNotMatter(World world, Box box) {
+    private Stream<BlockPos> skipFireTestIfResultDoesNotMatter(Box arg) {
         // Skip scanning the blocks around the entity touches by returning an empty stream when the result does not matter
         if (this.fireTicks > 0 || this.fireTicks == -this.getBurningDuration()) {
             return Stream.empty();
         }
 
-        return world.method_29556(box);
-    }*/
+        return BlockPos.stream(arg);
+    }
 }
